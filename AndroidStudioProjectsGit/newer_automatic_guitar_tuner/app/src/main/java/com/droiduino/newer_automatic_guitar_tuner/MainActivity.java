290867cbb50757126331c449729e3da8cf3a3499
Kenny Meier <kem243@pitt.edu>
2021-06-27 18:03:31 -0400

    Simplebluetooth

--- AndroidStudioProjectsGit/newer_automatic_guitar_tuner/app/src/main/java/com/droiduino/newer_automatic_guitar_tuner/MainActivity.java
+++ AndroidStudioProjectsGit/newer_automatic_guitar_tuner/app/src/main/java/com/droiduino/newer_automatic_guitar_tuner/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -31,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
 }
